450117a88abd114ab167758919418fb4a72ba637
Nilesh Roy <nileshroy404@gmail.com>
2025-12-15 21:11:50 +0530

    minor fix

--- src/core/todoItem.js
+++ src/core/todoItem.js
@@ -1,20 +1,20 @@
 // Default Blueprint of each Todo Item
 // (!) No state management
 
-class todoItem{
-  constructor(id, name, completed=false){
+export class todoItem {
+  constructor({ id, name, completed = false }) {
     this.id = id;
     this.name = name;
     this.completed = completed;
   }
-  
+
   // toggle the completed value (false <-> true)
-  toggle(){
+  toggle() {
     this.completed = !this.completed;
   }
-  
+
   // Update todo name through func parameter
-  update(changedName){
+  update(changedName) {
     this.name = changedName;
   }
 }
