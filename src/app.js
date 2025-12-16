c8cea25b3c10f8956746e46beb23482af3618ccc
Nilesh Roy <nileshroy404@gmail.com>
2025-12-16 19:42:10 +0530

    minor fix

--- src/app.js
+++ src/app.js
@@ -1,4 +1,36 @@
-import { todoItem } from "./core/todoItem.js";
+import { TodoState } from "./core/todoState.js";
 
-const todo = new todoItem(1, "Write Blog",);
-console.log(todo);
+// cupture userinput, call add() <- TodoState
+import { getTodo } from "./components/getTodo.js";
+ 
+
+const state = new TodoState();
+const newGetTodo = new getTodo(state);
+
+// const newTodoList = new todoList(state);
+console.log(newGetTodo);
+
+
+// import { Todo } from "./core/todo.js";
+// import { TodoState } from "./core/todoState.js";
+
+// let newTodo = new Todo({id:1234, name:"Ben"});
+// console.log(newTodo);
+
+console.log(new TodoState());
+
+export class getTodo {
+  constructor(state) {
+    this.state = state;
+    this.form = document.querySelector("#todo-form");
+    this.input = document.querySelector("#todo-input");
+    
+    this.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      if(!this.input.value.trim()) return
+      
+      this.state.add(this.input.value);
+      this.input.value = ""
+    })
+  }
+}
