0b8f43d508bdb2705d512d00737c6b8363fdc122
Nilesh Roy <nileshroy404@gmail.com>
2025-12-16 19:37:12 +0530

    add todo entity(domain model)

---
+++ src/core/todo.js
@@ -0,0 +1,21 @@
+// Default Blueprint a todo
+// (!) No state management
+
+export class Todo {
+  constructor({ id, name, completed = false }) {
+    this.id = id;
+    this.name = name;
+    this.completed = completed;     // false
+    
+  }
+
+  // toggle the value of completed by calling (false <-> true)
+  toggle() {
+    this.completed = !this.completed;
+  }
+
+  // Update todo name through func parameter
+  update(changedName) {
+    this.name = changedName;
+  }
+}
