6925f5335d8d7abe8b5462242100808e8f6d98fa
Nilesh Roy <nileshroy404@gmail.com>
2025-12-17 19:28:35 +0530

    connect states and components

--- src/app.js
+++ src/app.js
@@ -1,36 +1,11 @@
 import { TodoState } from "./core/todoState.js";
-
-// cupture userinput, call add() <- TodoState
-import { getTodo } from "./components/getTodo.js";
- 
+import { TodoFromController } from "./components/createTodo.js";  
+import { TodoList } from "./components/todoList.js";
 
 const state = new TodoState();
-const newGetTodo = new getTodo(state);
+new TodoFromController(state);
+new TodoList(state);
 
-// const newTodoList = new todoList(state);
-console.log(newGetTodo);
+console.log(new TodoList(state));
+console.log(state.todos); 
 
-
-// import { Todo } from "./core/todo.js";
-// import { TodoState } from "./core/todoState.js";
-
-// let newTodo = new Todo({id:1234, name:"Ben"});
-// console.log(newTodo);
-
-console.log(new TodoState());
-
-export class getTodo {
-  constructor(state) {
-    this.state = state;
-    this.form = document.querySelector("#todo-form");
-    this.input = document.querySelector("#todo-input");
-    
-    this.form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      if(!this.input.value.trim()) return
-      
-      this.state.add(this.input.value);
-      this.input.value = ""
-    })
-  }
-}
