7f1cd429adc404671dcca85634e70b28b2380f09
Nilesh Roy <nileshroy404@gmail.com>
2025-12-17 14:41:15 +0530

    add submit handler for creating todo

---
+++ src/components/createTodo.js
@@ -0,0 +1,17 @@
+export class TodoFromController {
+  constructor(state) {
+    this.state = state;
+    this.form = document.querySelector("#todo-form");
+    this.input = document.querySelector("#todo-input");
+    
+    this.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      
+      // if value is empty -> do nothing, return(stop)
+      if(!this.input.value.trim()) return
+      
+      this.state.add(this.input.value);
+      this.input.value = ""
+    })
+  }
+} 
