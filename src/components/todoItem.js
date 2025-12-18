2accaa03335a24dadd9da4509ceeb116c7129daf
Nilesh Roy <nileshroy404@gmail.com>
2025-12-18 13:44:01 +0530

    Fix bug for UI rendering

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -1,24 +1,38 @@
-export function TodoItem (todo){
-  const listItem = document.createElement('li');
-  
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
+export function TodoItem(todo, state) {
+  const listItem = document.createElement("li");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
   checkbox.checked = todo.completed;
+  console.log("Here", checkbox.checked);
+  checkbox.addEventListener("click", (e) => {
+    e.preventDefault();
+    state.checked(todo.id);
+    console.log("clicked", todo.id, checkbox.checked);
+  });
   
-  const span = document.createElement('span');
-  span.textContent = todo.name; 
-    
-  const icons = document.createElement('div');
-  icons.classList.add('itemIcon');
-  
-  const todoEdit = document.createElement('span');
+  const span = document.createElement("span");
+  span.textContent = `${todo.name}: ${todo.id}`;
+
+  const icons = document.createElement("div");
+  icons.classList.add("itemIcon");
+
+  const todoEdit = document.createElement("span");
   todoEdit.textContent = "edit";
-  const todoDel = document.createElement('span');
+  Object.assign(todoEdit.style, {
+    backgroundColor: "royalblue",
+    color: "white",
+    padding: "4px 10px",
+    borderRadius: "8px",
+    cursor: "pointer",
+  });
+
+  const todoDel = document.createElement("span");
   todoDel.textContent = "delete";
-  
+
   icons.append(todoEdit, todoDel);
-  
-  listItem.append(checkbox, span, icons)
-  
+
+  listItem.append(checkbox, span, icons);
+
   return listItem;
 }
--- src/components/todoList.js
+++ src/components/todoList.js
@@ -3,14 +3,14 @@ import { TodoItem } from "./todoItem.js";
 export class TodoList {
   constructor(state){
     this.todoList = document.querySelector('#todo-list');
-    console.log(this.todoList);
+    this.state = state;
     state.subscribe(this.render.bind(this));
     this.render(state.todos);
   }
   render(todos){
     this.todoList.innerHTML = '';
     todos.forEach(todo => {
-      this.todoList.appendChild(TodoItem(todo)); 
+      this.todoList.appendChild(TodoItem(todo, this.state)); 
     })
   }
 }
